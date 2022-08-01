1ef6015caab2af9d9c1005f8f5ad1374dd772552
antun.web <antun.web@gmail.com>
2022-08-01 04:04:23 -0400

    nove postavke sail-a

--- app/Http/Livewire/ShowFilm.php
+++ app/Http/Livewire/ShowFilm.php
@@ -39,7 +39,7 @@ class ShowFilm extends Component
             $url_parts = explode("/", $this->film['url']);
             $url_id = $url_parts[count($url_parts)-2];
 
-            $this->film = $this->getDataByFullUrl(env('APP_URL').'api/films/'.$url_id.'/show_all');
+            $this->film = $this->getDataByFullUrl(env('APP_URL').'/api/films/'.$url_id.'/show_all');
         }
 
 
